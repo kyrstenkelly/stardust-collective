71086e39257b1ed0f818fb8211212b43b18abd4e
Kyrsten Kelly <kyrsten@udacity.com>
2021-03-14 11:59:07 -0500

    feat: show user initials in avatar

--- components/common/Avatar/Avatar.tsx
+++ components/common/Avatar/Avatar.tsx
@@ -1,4 +1,5 @@
 import { FC, useRef } from 'react'
+import useCustomer from '@framework/customer/use-customer'
 import { useUserAvatar } from '@lib/hooks/useUserAvatar'
 import s from './Avatar.module.css'
 
@@ -8,11 +9,17 @@ interface Props {
 }
 
 const Avatar: FC<Props> = ({}) => {
-  let ref = useRef() as React.MutableRefObject<HTMLInputElement>
-  let { userAvatar } = useUserAvatar()
+  const ref = useRef() as React.MutableRefObject<HTMLInputElement>
+  const { userAvatar } = useUserAvatar()
+  const { data: customer } = useCustomer()
+  const firstInitial = customer?.firstName.charAt(0) || ''
+  const lastInitial = customer?.lastName.charAt(0) || ''
+  const initials = `${firstInitial}${lastInitial}`.toLowerCase()
 
   return (
-    <div ref={ref} style={{ background: userAvatar }} className={s.root}></div>
+    <div ref={ref} style={{ background: userAvatar }} className={s.root}>
+      <div>{initials}</div>
+    </div>
   )
 }
 
