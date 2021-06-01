10b80106f4d07276b66e905d1107d618744a4daf
Kyrsten Kelly <kyrsten@udacity.com>
2021-06-01 10:25:13 -0500

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const commerce = require('./commerce.config.json')
 const withCommerceConfig = require('./framework/commerce/with-config')
 
-const isBC = commerce.provider === 'bigcommerce'
-const isShopify = commerce.provider === 'shopify'
-
 module.exports = withCommerceConfig({
   commerce,
   i18n: {
@@ -12,17 +9,14 @@ module.exports = withCommerceConfig({
   },
   rewrites() {
     return [
-      (isBC || isShopify) && {
+      {
         source: '/checkout',
         destination: '/api/bigcommerce/checkout',
       },
-      // The logout is also an action so this route is not required, but it's also another way
-      // you can allow a logout!
-      isBC && {
+      {
         source: '/logout',
         destination: '/api/bigcommerce/customers/logout?redirect_to=/',
       },
-      // Rewrites for /shop
       {
         source: '/shop/:category',
         destination: '/shop',
