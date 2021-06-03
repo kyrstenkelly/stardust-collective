7e19692276af802c8eb2875005b300cdc679fe64
Kyrsten Kelly <kyrsten@udacity.com>
2021-06-03 11:43:53 -0500

    fix: adjust commission form height

--- pages/commissions.tsx
+++ pages/commissions.tsx
@@ -32,7 +32,7 @@ export default function Commissions() {
             src="https://airtable.com/embed/shreedeZhCWDKjsgI?backgroundColor=teal"
             frameBorder="0"
             width="100%"
-            height="2990"
+            height="1640"
             style={{ background: 'transparent', border: '1px solid #ccc' }}
           />
         </div>
